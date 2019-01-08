2978ec248ab645c0dd728189621e9040ef265b9e
Malcolm Akinje <malcolm.akinje@gmail.com>
2019-01-08 17:51:40 -0500

    Bot now responds with data that is going to be added to the db. Need to work on names with spaces in between.

---
+++ src/league_api.rs
@@ -0,0 +1,85 @@
+use serde_json;
+use serde_json::Error;
+use serde_derive::{Serialize, Deserialize};
+use reqwest;
+
+const SUMMONER_INFO_BY_NAME_ENDPOINT: &str = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
+const RANKED_INFO_BY_SUMMONER_ID_ENDPOINT : &str = "https://na1.api.riotgames.com/lol/league/v4/positions/by-summoner/";
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SummonerInfo {
+    profile_icon_id : i64,
+    pub name : String,
+    pub puuid : String,
+    pub summoner_level : i64,
+    revision_date : i64,
+    pub id : String, //Summoner ID(Encrypted)
+    account_id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RankedQueue {
+    pub queue_type : String,
+    summoner_name : String,
+    wins : i64,
+    losses : i64,
+    league_id : String,
+    pub rank : String,
+    league_name : String,
+    pub tier : String,
+    summoner_id : String,
+    league_points : i64,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct MiniSeriesDTO {
+    wins : i64,
+    losses : i64,
+    target : i64,
+    progress : String,
+}
+
+pub struct Client {
+    api_key : String,
+}
+
+pub fn new_client(api_key : String) -> Client {
+    Client {
+        api_key: api_key,
+    }
+}
+
+fn build_endpoint(client: &Client, base: &str, end: &String) -> Box<String> {
+    let mut endpoint = String::from(base);
+    endpoint.push_str(&end);
+    endpoint.push_str("?api_key=");
+    endpoint.push_str(client.api_key.as_ref());
+    Box::new(endpoint)
+    
+}
+
+impl Client {
+
+    pub fn get_ranked_info(&self, summoner_name: &String) -> Vec<RankedQueue> {
+        let endpoint = build_endpoint(self, SUMMONER_INFO_BY_NAME_ENDPOINT,
+                                        &summoner_name);
+        let endpoint_ref: &str = endpoint.as_ref();
+        let response = reqwest::get(endpoint_ref).unwrap().text().unwrap();
+        println!("response: {:?}", response);
+        let summoner_info: SummonerInfo = serde_json::from_str(&response).unwrap();
+
+        let ranked_endpoint = build_endpoint(&self, RANKED_INFO_BY_SUMMONER_ID_ENDPOINT,
+                                     &summoner_info.id);
+        let ranked_endpoint_ref: &str = ranked_endpoint.as_ref();
+        let response = reqwest::get(ranked_endpoint_ref).unwrap().text().unwrap();
+        println!("response: {:?}", response);
+        let ranked_info: Vec<RankedQueue> = serde_json::from_str(&response).unwrap();
+        ranked_info
+    }
+}
+
+impl SummonerInfo {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-#[macro_use] extern crate serenity;
+#[macro_use]
+extern crate serenity;
+extern crate serde;
+extern crate serde_derive;
 
-use serenity::client::{
-    Client,
-    EventHandler,
-};
+use serenity::client::{Client, EventHandler};
 
+use serenity::framework::standard::StandardFramework;
 use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
-use serenity::framework::standard::StandardFramework;
 use std::env;
+use reqwest;
+
+pub mod league_api;
 
 struct Handler;
 
@@ -35,6 +38,14 @@ impl Game {
             _ => None,
         }
     }
+
+    fn to_string(&self) -> &'static str {
+        match self {
+            Game::Dota => "Dota 2",
+            Game::Lol => "League of Legends",
+            Game::Wow => "World of Warcraft",
+        }
+    }
 }
 
 pub fn main() {
@@ -42,10 +53,13 @@ pub fn main() {
     let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
         .expect("Error creating client");
 
-    client.with_framework(StandardFramework::new()
-        .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
-        .cmd("commands", commands)
-        .cmd("lfg", lfg));
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("!")) // set the bot's prefix to "~"
+            .cmd("commands", commands)
+            .cmd("lfg", lfg)
+            .cmd("ping", ping),
+    );
 
     // start listening for events by starting a single shard
     if let Err(why) = client.start() {
@@ -59,12 +73,57 @@ command!(commands(_ctx, message, _args) {
 });
 
 command!(lfg(_ctx, message, _args) {
-    let mut response = String::from("You are looking for a ");
-    let game = Game::new(_args.single::<String>().unwrap());
-    match game {
-        Some(Game::Dota) => message.reply("You are looking for a Dota2 game"),
-        Some(Game::Lol)  => message.reply("You are looking for a LoL game"),
-        Some(Game::Wow)  => message.reply("You are looking for a WoW game"),
-        None             => message.reply("Sorry we don't have functionality for that game yet"),
-    };
+    let game = match Game::new(_args.single::<String>().unwrap()) {
+                Some(g) => g,
+                None => {
+                            message.reply("Sorry that game is not implemented yet")?;
+                            return Ok(())
+                        }
+                };
+    let summoner_name = _args.single::<String>().unwrap();
+    let api_key = match env::var("RIOT_API_KEY") {
+                        Ok(key) => key,
+                        Err(e) => panic!(e),
+                    };
+    let client = league_api::new_client(api_key);
+    let ranked_info = client.get_ranked_info(&summoner_name);
+    let mut index = 0;
+    for i in 0..ranked_info.len() {
+        if ranked_info[i].queue_type == "RANKED_SOLO_5x5" {
+            index = i;
+        }
+    }
+
+    let reply_msg = construct_lfg_reply(summoner_name, &ranked_info[index], &message, game);
+    message.reply(&(*reply_msg))?;
+});
+
+fn construct_lfg_reply(summoner_name: String, ranked_info: &league_api::RankedQueue, msg: &Message, game: Game) -> Box<String> {
+    let mut reply = String::from("```This is the info being added to the database:\n");
+    reply.push_str("Summoner-Name : ");
+    reply.push_str(&summoner_name);
+    reply.push_str("\n");
+    reply.push_str("Discord-Name : ");
+    reply.push_str(&msg.author.name);
+    reply.push_str("\n");
+    reply.push_str("Game: ");
+    reply.push_str(game.to_string());
+    reply.push_str("\n");
+    reply.push_str("Rank: ");
+    reply.push_str(&ranked_info.tier);
+    reply.push_str("\n```");
+    Box::new(reply)
+}
+
+command!(ping(_ctx, message, _args) {
+//    let client = redis::Client::open("redis-17469.c14.us-east-1-3.ec2.cloud.redislabs.com:17469");
+//    let con = client.get_connection().unwrap();
+//    let _ : () = con.set("my_key", 42)?;
+//    match con.get("my_key") {
+//        Ok(v) => {
+//            let val : u32 = v;
+//            println!("worked");
+//            },
+//        Err(why) => println!("Error: {}", why),
+//    }
 });
